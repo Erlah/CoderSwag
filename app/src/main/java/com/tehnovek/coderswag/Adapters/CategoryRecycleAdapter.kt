3a59ff161068e24a092e903724abf139003acba9
Vladimir Sokolov <sokolovva@outlook.com>
2019-08-06 00:00:33 +0500

    lesson 05

--- app/src/main/java/com/tehnovek/coderswag/Adapters/CategoryRecycleAdapter.kt
+++ app/src/main/java/com/tehnovek/coderswag/Adapters/CategoryRecycleAdapter.kt
@@ -11,11 +11,11 @@ import com.tehnovek.coderswag.Model.Category
 import com.tehnovek.coderswag.R
 import kotlinx.android.synthetic.main.category_list_item.view.*
 
-class CategoryRecycleAdapter(val context: Context, val categories: List<Category>) :
+class CategoryRecycleAdapter(val context: Context, val categories: List<Category>, val itemClick: (Category) -> Unit ) :
     RecyclerView.Adapter<CategoryRecycleAdapter.Holder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val view = LayoutInflater.from(context).inflate(R.layout.category_list_item, parent, false)
-        return Holder(view)
+        return Holder(view, itemClick)
     }
 
     override fun getItemCount() = categories.count()
@@ -24,7 +24,7 @@ class CategoryRecycleAdapter(val context: Context, val categories: List<Category
         holder?.bindCategory(categories[position], context)
     }
 
-    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class Holder(itemView: View, val itemClick: (Category) -> Unit) : RecyclerView.ViewHolder(itemView) {
         val categoryImage = itemView?.findViewById<ImageView>(R.id.categoryImage)
         val categortyName = itemView?.findViewById<TextView>(R.id.categoryName)
 
@@ -32,6 +32,7 @@ class CategoryRecycleAdapter(val context: Context, val categories: List<Category
             val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
             categoryImage?.setImageResource(resourceId)
             categortyName?.text = category.title
+            itemView.setOnClickListener { itemClick(category) }
         }
     }
 }
--- app/src/main/java/com/tehnovek/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/tehnovek/coderswag/Controller/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tehnovek.coderswag.Controller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -10,6 +11,7 @@ import com.tehnovek.coderswag.Adapters.CategoryRecycleAdapter
 import com.tehnovek.coderswag.Model.Category
 import com.tehnovek.coderswag.R
 import com.tehnovek.coderswag.Services.DataService
+import com.tehnovek.coderswag.Utilities.EXTRA_CATEGORY
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,7 +22,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryRecycleAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories) {
+            val productIntent = Intent(this, ProductsActivity::class.java)
+            productIntent.putExtra(EXTRA_CATEGORY, it.title)
+            startActivity(productIntent)
+        }
         categoryListView.adapter = adapter
 
         val layoutManger = LinearLayoutManager(this)
